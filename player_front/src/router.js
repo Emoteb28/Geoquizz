d9da3e79111330c95894e5bac96de9a0e0065cb1
Mouad MOUNACH <me.mounach@gmail.com>
2019-03-13 20:07:53 +0100

    player front

--- player_front/src/router.js
+++ player_front/src/router.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Login from './views/Login.vue'
 
 Vue.use(Router)
-
-axios.defaults.baseURL = 'http://api.backoffice.local:10080/'
 
 export default new Router({
   routes: [
@@ -21,11 +18,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path : '/login[/]',
-      name: 'login',
-      component : Login
     }
   ]
 })
