184c7a635548ccd23e7800578829affe57a2dbb1
Salah <salah-57100@hotmail.fr>
2019-03-13 19:05:42 +0100

    Page login + actions -> store

--- backoffice/backoffice_app/src/router.js
+++ backoffice/backoffice_app/src/router.js
@@ -5,25 +5,19 @@ import Login from './views/Login.vue'
 
 Vue.use(Router)
 
-axios.defaults.baseURL = 'http://api.backoffice.local:10080/'
+
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path : '/login[/]',
+      path : '/login',
       name: 'login',
       component : Login
     }
