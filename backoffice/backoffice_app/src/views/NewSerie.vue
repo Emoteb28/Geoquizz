5b8009e786179b7bd4795ba0784c7790e1a3ba3f
Salah <salah-57100@hotmail.fr>
2019-03-20 18:27:57 +0100

    push2

--- backoffice/backoffice_app/src/views/NewSerie.vue
+++ backoffice/backoffice_app/src/views/NewSerie.vue
@@ -1,27 +1,44 @@
 <template>
-  <v-container class="slide-bck-top">
-    <h6 class="display-3">Créer une nouvelle série</h6>
-    <v-form @submit.prevent="createSerie" ref="form">
-      <v-text-field v-model="city" label="Ville" required></v-text-field>
+  <v-container>
+    <div>
+      <v-container class="slide-bck-top">
+        <h6 class="display-3">Créer une nouvelle série</h6>
+        <v-form @submit.prevent="createSerie" ref="form">
+          <v-text-field v-model="city" label="Ville" required></v-text-field>
 
-      <v-text-field v-model="lat" label="Latitude" required></v-text-field>
+          <v-text-field v-model="lat" label="Latitude" required></v-text-field>
 
-      <v-text-field v-model="lon" label="Longitude" required></v-text-field>
-      <v-text-field v-model="dist" label="Distance" required></v-text-field>
+          <v-text-field v-model="lon" label="Longitude" required></v-text-field>
+          <v-text-field v-model="dist" label="Distance" required></v-text-field>
 
-      <v-btn color="success" type="submit">Envoyer</v-btn>
-    </v-form>
+          <v-btn color="success" type="submit">Envoyer</v-btn>
+        </v-form>
+      </v-container>
+    </div>
+
   </v-container>
 </template>
 
+
 <script>
+import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
 export default {
-  name: "FilmForm",
   data: () => ({
     city: "",
     lat: null,
     lon: null,
-    dist: null
+    dist: null,
+    zoom: 13,
+    center: L.latLng(30, 30),
+    url: "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    marker: L.latLng(30, 30),
+    drag: true,
+    photoIndex: 0,
+    time: 0,
+    interval: false,
+    score: 0
   }),
   methods: {
     createSerie() {
@@ -33,55 +50,41 @@ export default {
           dist: this.dist
         })
         .then(response => {
-          this.$router.push({ name: 'series' })
+          this.$router.push({ name: "series" });
         });
     }
+  },
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
   }
 };
 </script>
 
 <style lang="scss">
-#card {
-  margin-left: 15%;
-  margin-top: 5%;
+#map {
+  height: 100%;
+  max-width: 100%;
+  margin: auto;
+  padding: 0;
+  border-radius: 5%;
 }
-a {
-  text-decoration: none;
-}
-
-
-
-
-
-/**
- * ----------------------------------------
- * animation slide-bck-top
- * ----------------------------------------
- */
-.slide-bck-top {
-
-	-webkit-animation: slide-bck-top 0.45s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
-	        animation: slide-bck-top 0.45s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
-}
-@-webkit-keyframes slide-bck-top {
-  0% {
-    -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
-  }
-  100% {
-    -webkit-transform: translateZ(-400px) translateY(-200px);
-            transform: translateZ(-400px) translateY(-200px);
-  }
-}
-@keyframes slide-bck-top {
-  0% {
-    -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
-  }
-  100% {
-    -webkit-transform: translateZ(-400px) translateY(50px);
-            transform: translateZ(-400px) translateY(50px);
-  }
+.top2 {
+  margin-top: 2%;
+  font-size: 1.3em;
 }
 
+#row,
+#row2 {
+  width: 95%;
+  margin-left: 2%;
+  margin-top: 3%;
+}
+
+#col1,
+#col2 {
+  margin: 0;
+  padding: 0;
+}
 </style>
